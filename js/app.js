7d79a977ea1882851607b4bab269128a2a475215
himel1442 <himelahmed1442@gmail.com>
2023-12-17 16:42:10 +0600

    tufaelTEX

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$('.destination_part').slick({
+  slidesToShow: 5,
+  slidesToScroll: 3,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: false,
+  centerMode: true,
+  centerPadding: 0,
+});
+
+$('.counter_plgin').counterUp({
+  delay: 10,
+  time: 2000,
+});
